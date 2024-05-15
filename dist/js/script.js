70e08fe55037dc7ba8e65c73a526198a8ba5f964
yuki <snkyh.boc@gmail.com>
2024-05-15 10:49:42 +0900

    3回目のプッシュです。

--- dist/js/script.js
+++ dist/js/script.js
@@ -36,3 +36,24 @@ jQuery(function ($) {
   });
 
 });
+
+// フェードアップ
+$('.animate__animated').waypoint({
+  handler: function(direction) {
+    // 要素が画面中央に来るとここが実行される
+    if (direction === 'down') {
+      /**
+       * 下方向のスクロール
+       * イベント発生元の要素に
+       * fadeInLeftアニメーションを付けることで
+       * アニメーションを開始
+       */
+      $(this.element)
+        .addClass('animate__fadeInUp');
+    }
+  },
+  
+  // 要素が画面中央に来たらhandlerを実行
+  offset: '50%',
+});
+
